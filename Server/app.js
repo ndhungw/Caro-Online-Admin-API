30b62032ed2a60d5be749db2c08964b97dd981ef
Nguyen Dong Hung <ndh1379@gmail.com>
2021-01-11 13:11:23 +0700

    add cors

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const passport = require("passport");
+const cors = require("cors");
 
 require("dotenv").config();
 // connect database
@@ -14,6 +15,7 @@ const authRouter = require("./routes/auth");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
